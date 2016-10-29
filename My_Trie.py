d536968e4c79d2c7425747890b940266595b8433
Yask Srivastava <yask123@gmail.com>
2016-10-29 14:04:13 +0530

    Trie with insertion, searching, prefix search for words (supports a-z)

--- My_Trie.py
+++ My_Trie.py
@@ -1,32 +1,65 @@
 class TrieNode:
-    def __init__(self):
-        self.links = [None]*26
-        self.is_end = False
+	def __init__(self):
+		self.links = [None] * 26
+		self.is_end = False
 
-    def is_char_present(self, char):
-        return self.links[ord('a') - ord(char)] != None
+	def add_link(self, character):
+		new_trie_node = TrieNode()
+		self.links[ord(character) - ord('a')] = new_trie_node
 
-    def is_terminal(self):
-        return self.is_end
+	def get_link(self, character):
+		return self.links[ord(character) - ord('a')]
 
-    def set_terminal(self):
-        self.is_end = True
-
-    def get_node(self, char):
-        return self.links[ord('a')-ord(char)]
-
-    def add_node(self, char):
-        self.links[ord['a'] - ord(char)] = char
 
 class Trie:
-    def __init__(self):
-        self.root = TrieNode
+	def __init__(self):
+		self.root = TrieNode()
 
-    def insert_string(self, input_string):
-        currnet = self.root
-        index = 0
-        while currnet.is_char_present(input_string[index]):
-            currnet = currnet.get_node(input_string[index])
-            index += 1
+	def add_word(self, word):
+		current = self.root
 
-        currnet.add_node(c)
+		for each_char in word:
+			if current.get_link(each_char) == None:
+				current.add_link(each_char)
+
+			current = current.get_link(each_char)
+		current.is_end = True
+
+	def search_word(self, word):
+		current = self.root
+
+		for each_char in word:
+			if current.get_link(each_char) == None:
+				return False
+			current = current.get_link(each_char)
+		if current.is_end == True:
+			return True
+
+		return False
+
+	def words_with_prefixes(self, word):
+		current = self.root
+		for each_char in word:
+			current = current.get_link(each_char)
+			if current == None:
+				return None
+		self.dfs(current, '', word)
+
+	def dfs(self, node, result, word):
+		if node == None:
+			return
+
+		if node.is_end:
+			print word + result
+
+		for i in range(26):
+			if (node.links[i] != None):
+				self.dfs(node.links[i], result + chr(ord('a') + i), word)
+
+
+my_trie = Trie()
+
+my_trie.add_word('yask')
+my_trie.add_word('yahoo')
+my_trie.add_word('hello')
+my_trie.words_with_prefixes('yah')
