d810ddbdfb3b10711030b136307614e38626bf32
Yask Srivastava <yask123@gmail.com>
2016-11-03 23:24:21 +0530

    Convert a binary tree to double linked list (using static variable, hacky way of passing values of reference :|

--- tree2list.py
+++ tree2list.py
@@ -1,46 +1,48 @@
+def tree_to_ll(root):
+    if root == None:
+        return
+
+    tree_to_ll(root.left)
+
+    if tree_to_ll.prev == None:
+        tree_to_ll.head = root
+    else:
+        tree_to_ll.prev.right = root
+
+    tree_to_ll.prev = root
+
+    tree_to_ll(root.right)
+
+
+def convert_tree_to_linked_list(root):
+    tree_to_ll.prev = None
+    tree_to_ll.head = None
+
+    tree_to_ll(root)
+
+    print_linked_list(tree_to_ll.head)
+
+
+def print_linked_list(root):
+    current = root
+    while current:
+        print current.val, '-->',
+        current = current.right
+
+
 class Node:
     def __init__(self, data):
-        self.data = data
-        self.left = None
         self.right = None
+        self.left = None
+        self.val = data
 
-a  = Node('a')
-b  = Node('b')
-c  = Node('c')
 
-a.right = c
+a = Node(5)
+b = Node(1)
+c = Node(8)
+
 a.left = b
 
+a.right = c
 
-def tree_to_list(root, prev_node, head_node, last_item_visited):
-    if root == None:
-        return None
-
-    tree_to_list(root.left, prev_node, head_node, last_item_visited)
-
-    if prev_node[0]:
-        prev_node[0].right = root
-        root.left = prev_node[0]
-    else:
-        head_node.append(root)
-
-    last_item_visited.pop()
-    last_item_visited.append(root)
-
-    prev_node.pop()
-    prev_node.append(root)
-
-    tree_to_list(root.right, prev_node, head_node, last_item_visited)
-
-current = b
-
-head_node = []
-last_item_visited = [None]
-
-tree_to_list(a, [None], head_node, last_item_visited)
-
-current = head_node[0]
-last_item_visited[0].right = head_node[0]
-head_node[0].prev = last_item_visited[0]
-
-current = head_node[0]
+convert_tree_to_linked_list(a)
