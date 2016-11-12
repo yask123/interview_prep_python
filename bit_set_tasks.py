69e615d3a9880e2e76229d2eaaefcf567a56cd66
Yask Srivastava <yask123@gmail.com>
2016-11-12 14:11:27 +0530

    O(1) to set ith bit in any number

--- bit_set_tasks.py
+++ bit_set_tasks.py
@@ -4,3 +4,11 @@ def get_bit(n, i):
 					'''
 	# Left shift of 1 by i positions
 	return n & (1 << i) != 0
+
+
+def set_bit(n, i):
+	'''
+	Sets the ith bit of n (from left) to 1
+	4 -> 100 -> set_bit(4,0) -> 101 -> 5
+	'''
+	return n | 1 << i
