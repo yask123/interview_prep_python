6b149eeeb02d05c0680671f367a96a91c912af07
Yask Srivastava <yask123@gmail.com>
2016-11-16 23:28:07 +0530

    Rotate LinkedList

--- max_depth_of_tree.py
+++ max_depth_of_tree.py
@@ -1,8 +1,42 @@
-def max_depth(root):
-    if root == None:
-        return 0
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-    left_subtree_depth = max_depth(root.left)
-    right_subtree_depth = max_depth(root.right)
 
-    return max(left_subtree_depth, right_subtree_depth) +1
+class Solution:
+    # @param A : root node of tree
+    # @param B : integer
+    # @return the root node in the tree
+    def getSuccessor(self, A, B):
+        current = A
+        successor = None
+        while True:
+            if current.val == B:
+                if current.right:
+                    return self.get_min(A.right)
+                else:
+                    return successor
+            elif current.val < B:
+                current = current.right
+            else:
+                successor = current
+                current = current.left
+
+    def get_min(self, A):
+        while A.left:
+            A = A.left
+        return A
+
+
+a = TreeNode(2)
+b = TreeNode(1)
+c = TreeNode(3)
+
+a.right = c
+a.left = b
+
+ans = Solution()
+print ans.getSuccessor(a, 1).val
---
+++ rotate_linked_list.py
@@ -0,0 +1,26 @@
+# Rotate linked list
+'''
+Concept: New head will be N - K th element. Set prev of (n-k)th to None and set the tail of linked list to prev head
+O(n)
+'''
+
+
+def rotate(self, A, B):
+	total_length = self.get_length(A)
+	prev_head = A
+	B = B % total_length
+	prev = None
+	for _ in range(total_length - B):
+		prev = A
+		A = A.next
+	if prev != None and A != None:
+		prev.next = None
+		head = A
+
+		while A.next != None:
+			A = A.next
+		A.next = prev_head
+
+		return head
+	else:
+		return prev_head
