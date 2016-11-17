236679c2d14b5bf502549e8a8fb3f47aac308cc6
Yask Srivastava <yask123@gmail.com>
2016-11-17 21:12:08 +0530

    subset sum problem top down aproach

--- subset_sum.py
+++ subset_sum.py
@@ -1,17 +1,36 @@
+'''
+subset sum problem, top-down aproach.
+Target sum = T
+Length of array = N
+
+Optimized time complexity after memoization : O(T*N) (Brought down from exponential)
+Space : O(T*N)
+'''
+
 arr = [1, 2, 3, 4]
 
 
-def sub_set_sum(arr, current_index, target, ans):
+def subset_sum(arr, index, target, current_result, cache={}):
+	if index not in cache:
+		cache[index] = {}
+	if target in cache[index]:
+		return cache[index][target]
+
 	if target == 0:
-		print (ans)
-		return
-	if target < 0:
-		return
-	if current_index < 0:
-		return 
+		return current_result
+	if target < 0 or index < 0:
+		return False
 
-	sub_set_sum(arr, current_index, target-arr[current_index], ans + str(arr[current_index]))
-	sub_set_sum(arr, current_index-1, target, ans)
+	a = subset_sum(arr, index - 1, target - arr[index], current_result + str(arr[index]), cache)
+	b = subset_sum(arr, index - 1, target, current_result, cache)
+
+	if a:
+		cache[index][target] = a
+		return cache[index][target]
+
+	if b:
+		cache[index][target] = b
+		return cache[index][target]
 
 
-sub_set_sum(arr, len(arr)-1, 5, '')
+print subset_sum(arr, len(arr) - 1, 5, '')
