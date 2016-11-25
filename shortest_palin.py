f6c615c43502a8bf44dea801f3418002f0e31f70
Yask Srivastava <yask123@gmail.com>
2016-11-25 21:22:35 +0530

    comeback

---
+++ shortest_palin.py
@@ -0,0 +1,30 @@
+'''
+O(n^2) solution, can be improved to O(n)
+'''
+
+
+def shortestPalindrome(s):
+    """
+    :type s: str
+    :rtype: str
+    """
+    index = 0
+    while not_palin(s):
+        s = s[:index] + s[len(s) - index - 1] + s[index:]
+        index += 1
+    return s
+
+
+def not_palin(s):
+    i = 0
+    j = len(s) - 1
+    while i < j:
+        if s[i] != s[j]:
+            return True
+        i += 1
+        j -= 1
+    return False
+
+
+inp = 'somerandomstring'
+print shortestPalindrome(inp)
