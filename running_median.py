84fe14caa20b2461499d4e9a336c6edb1d6f486c
Yask Srivastava <yask123@gmail.com>
2016-11-30 20:42:10 +0530

    Running median in n log(n) time

---
+++ running_median.py
@@ -0,0 +1,32 @@
+import heapq
+
+
+def running_median(arr):
+	min_heap = []
+	max_heap = []
+
+	for i in range(len(arr)):
+		if len(min_heap) == 0 or arr[i] < max_heap[0] * -1:
+			heapq.heappush(max_heap, -1 * arr[i])
+		else:
+			heapq.heappush(min_heap, arr[i])
+
+		while abs(len(max_heap) - len(min_heap)) > 1:
+			if len(max_heap) > len(min_heap):
+				popped_item = heapq.heappop(max_heap) * -1
+				heapq.heappush(min_heap, popped_item)
+			else:
+				popped_item = heapq.heappop(min_heap)
+				heapq.heappush(max_heap, popped_item * -1)
+
+	if len(min_heap) > len(max_heap):
+		return min_heap[0]
+	elif len(max_heap) > len(min_heap):
+		return -1 * max_heap[0]
+	else:
+		return (max_heap[0] * -1 + min_heap[0]) / 2
+
+
+arr = [1, 2, 3, 4, 5, 6, 7, 100, 101]
+
+print running_median(arr)
